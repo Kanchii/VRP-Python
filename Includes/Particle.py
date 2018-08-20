80d75bb043b04ce35294b6862972373361e5bd9e
Weiss <weisslipe@gmail.com>
2018-08-20 16:04:21 -0300

    Bah

--- Includes/Particle.py
+++ Includes/Particle.py
@@ -25,9 +25,6 @@ class Particle:
         self.velocidade = self.inercia * self.velocidade\
                         + self.fator_cognitivo * random_1 * (self.pbest - self.posicao)\
                         + self.fator_social * random_2 * (self.gbest - self.posicao)
-        # print(self.inercia * self.velocidade)
-        # print(self.fator_cognitivo * random_1 * (self.pbest - self.posicao))
-        # print(self.fator_social * random_2 * (self.gbest - self.posicao))
 
     def updatePosicao(self, matriz_distancias, demanda_clientes):
         self.posicao += self.velocidade
@@ -53,3 +50,18 @@ class Particle:
             seq.append(data[1])
         fitness += matriz_distancias[seq[-1]][0]
         return fitness
+
+    def OPT2(self, qtd, matriz_distancias, demanda_clientes):
+        import random, copy
+        cnt = 0
+        while(cnt < qtd):
+            f, s = random.randint(0, self.qtd_clientes - 1), random.randint(0, self.qtd_clientes - 1)
+            self.posicao[f], self.posicao[s] = self.posicao[s], self.posicao[f]
+            fit = self.calcFitness(matriz_distancias, demanda_clientes)
+            if(fit < self.pbest_fitness):
+                self.pbest = copy.deepcopy(self.posicao)
+                self.pbest_fitness = fit
+                cnt = 0
+            else:
+                self.posicao[f], self.posicao[s] = self.posicao[s], self.posicao[f]
+                cnt += 1
--- main.py
+++ main.py
@@ -8,9 +8,21 @@ capacidade_max, qtd_clientes, demanda_clientes, maxi_caminhoes, posicoes, matriz
 particles = []
 
 for _ in range(MAX_PARTICLES):
-    particles.append(Particle(capacidade_max, qtd_clientes))
+    particles.append(Particle(capacidade_max, qtd_clientes, 1.2, 1.5, 2.5))
 
-for _ in range(20):
-    print(particles[0].velocidade[0])
-    particles[0].updateVelocidade()
-    particles[0].updatePosicao(matriz_distancias, demanda_clientes)
+for _ in range(10000):
+    best = None
+    bestFit = 1e9
+    for i in range(MAX_PARTICLES):
+        particles[i].updateVelocidade()
+        particles[i].updatePosicao(matriz_distancias, demanda_clientes)
+        if(bestFit > particles[i].pbest_fitness):
+            bestFit = particles[i].pbest_fitness
+            best = particles[i].pbest
+    for i in range(MAX_PARTICLES):
+        particles[i].gbest = best
+    if(_ % 10 == 0):
+        for i in range(MAX_PARTICLES):
+            particles[i].OPT2(20, matriz_distancias, demanda_clientes)
+    if(_ % 100 == 0):
+        print(bestFit)
